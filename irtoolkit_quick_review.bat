1a0d7a110e93eb123f9d361ef54e17503af907f8
Gary Κ <33410501+g4xyk00@users.noreply.github.com>
2019-07-19 14:19:19 +0800

    Add files via upload

---
+++ irtoolkit_quick_review.bat
@@ -0,0 +1,19 @@
+echo off
+cls
+echo [*] Logon Session
+echo [*] Logon (Success)
+type le_logon_session_4624.txt | findstr /C:"Date" /C:"Logon Type" 
+@echo:
+echo [*] Logon (Failure)
+type le_logon_session_4625.txt | findstr /C:"Date" /C:"Source Network Address" 
+@echo: 
+@echo: 
+echo [*] Schedule Tasks
+type st_schtasks.txt | findstr /C:"/20
+@echo: 
+@echo: 
+echo [*]  User Account Changes
+type ac_net_user.txt
+@echo: 
+@echo: 
+pause
