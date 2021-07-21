a97789cf5510aa0b35e87fc1547e678729644c99
Gary Κ <33410501+g4xyk00@users.noreply.github.com>
2021-07-21 09:29:35 +0800

    Update inventory_wininfo.bat

--- inventory_wininfo.bat
+++ inventory_wininfo.bat
@@ -14,17 +14,20 @@ cd %folder%
 
 copy C:\Windows\System32\wbem\en-US\htable.xsl C:\Windows\system32\wbem /Y
 
-echo [+] Generating report for User Account (1/4)
+echo [+] Generating report for User Account (1/5)
 wmic /output:acc_all.html useraccount get AccountType,Caption,Description,FullName,Disabled /format:htable
 
-echo [+] Generating report for All Services (2/4)
+echo [+] Generating report for All Services (2/5)
 wmic /output:service_all.html service get DisplayName,Description,PathName,State,StartName /format:htable
 
-echo [+] Generating report for Running Services (3/4)
+echo [+] Generating report for Running Services (3/5)
 wmic /output:service_run.html service where state="Running" get DisplayName,Description,PathName,StartName /format:htable
 
-echo [+] Generating report for Software (4/4)
+echo [+] Generating report for Software (4/5)
 wmic /output:software_all.html product get name,version,Installsource,InstallDate,InstallDate2,LocalPackage /format:htable
+
+echo [+] Generating report for System (5/5)
+systeminfo > system.txt
 
 del C:\Windows\system32\wbem\htable.xsl
 
